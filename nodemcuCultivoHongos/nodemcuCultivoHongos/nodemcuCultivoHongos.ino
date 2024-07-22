7b1612b9f0240f24784ae5bec0a3c0af8bcc3598
gpabloandres <gpabloandres@gmail.com>
2024-07-22 14:07:44 -0300

    Agrega funcionalidad de control vía MQTT de led y bomba

--- nodemcuCultivoHongos/nodemcuCultivoHongos/nodemcuCultivoHongos.ino
+++ nodemcuCultivoHongos/nodemcuCultivoHongos/nodemcuCultivoHongos.ino
@@ -13,7 +13,7 @@ const char* ssid = "ASP";
 const char* password = "ASP110110";
 
 // Servidor MQTT
-const char* mqtt_server = "192.168.1.17";
+const char* mqtt_server = "192.168.1.12";
 
 // Credenciales del broker MQTT (pon NULL si no se requieren)
 const char* MQTT_username = NULL; 
